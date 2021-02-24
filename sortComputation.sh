6ba76da436c74685ef552c922b081ac75d1ec5db
rishabh0407 <rishabhinvincible123@gmail.com>
2021-02-24 13:16:08 +0530

    dicitionary added

--- sortComputation.sh
+++ sortComputation.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -x
 
+declare -A compute
 read -p "Enter 1st number " a
 read -p "Enter 2nd number " b
 read -p "Enter 3rd number " c
-echo g=$(((( $a % $b )) + $c ));
+d=$(( $a + (( $b * $c ))));
+e=$(((( $a * $b )) + $c));
+f=$(( $c + (( $a / $b ))));
+g=$(((( $a % $b )) + $c ));
+compute=( ["1st"]=$d ["2nd"]=$e ["3rd"]=$f ["4th"]=$g )
+echo "${compute[@]}"
