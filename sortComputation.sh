f54e3b0710416468f5e4fd99645ca26f02a7a9ec
rishabh0407 <rishabhinvincible123@gmail.com>
2021-02-24 13:43:09 +0530

    Array added

--- sortComputation.sh
+++ sortComputation.sh
@@ -9,4 +9,9 @@ e=$(((( $a * $b )) + $c));
 f=$(( $c + (( $a / $b ))));
 g=$(((( $a % $b )) + $c ));
 compute=( ["1st"]=$d ["2nd"]=$e ["3rd"]=$f ["4th"]=$g )
-echo "${compute[@]}"
+counter=0
+Computation[((counter++))]=${compute["1st"]}
+Computation[((counter++))]=${compute["2nd"]}
+Computation[((counter++))]=${compute["3rd"]}
+Computation[((counter++))]=${compute["4th"]}
+echo ${Computation[@]}
